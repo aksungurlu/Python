7593f803bc1d651c1aacfdeea0d0f5fd84009a9d
aksungurlu <t.aksungurlu@gmail.com>
2016-12-06 01:55:49 +0300

    keystroke completed

--- threaded.py
+++ threaded.py
@@ -2,10 +2,15 @@
 
 import threading
 import time
+import tty
 import sys
+import termios
 
 exitFlag = 0
 key = 'n'
+orig_settings = termios.tcgetattr(sys.stdin)
+
+
 
 class timerThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -37,17 +42,27 @@ class keystrokeThread (threading.Thread):
         
 def keystroke_handler(threadName):
     global key
+    global termios
+    
+    
 	
-    while True:
-        """if exitFlag:
-            threadName.exit()"""
-        key = sys.stdin.read( 1 )
-
+    while key != 'q':
+        if exitFlag:
+            threadName.exit()
+        tty.setraw(sys.stdin)
+        ch = sys.stdin.read( 1 )[0]
+        if ch != key:
+			key = ch
+	
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings) 
+			
 # Create new threads
 timer_5sn = timerThread(1, "timer 5 sn", 5)
+keystroke_h1 = keystrokeThread(2, "keystroke handler")
 
 # Start new Threads
 timer_5sn.start()
+keystroke_h1.start()
 
 print "hello world"
 print "how are you"

a8bccba2fa19f20c93a59b64b16c34d64964e1d3
aksungurlu <t.aksungurlu@gmail.com>
2016-12-06 01:36:48 +0300

    keystroek thread is ready

--- threaded.py
+++ threaded.py
@@ -2,8 +2,10 @@
 
 import threading
 import time
+import sys
 
 exitFlag = 0
+key = 'n'
 
 class timerThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -22,6 +24,24 @@ def start_timer(threadName, counter):
             threadName.exit()
         time.sleep(1)
         counter -= 1
+        
+class keystrokeThread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        print "Starting " + self.name
+        keystroke_handler(self.name)
+        print "Exiting " + self.name
+        
+def keystroke_handler(threadName):
+    global key
+	
+    while True:
+        """if exitFlag:
+            threadName.exit()"""
+        key = sys.stdin.read( 1 )
 
 # Create new threads
 timer_5sn = timerThread(1, "timer 5 sn", 5)

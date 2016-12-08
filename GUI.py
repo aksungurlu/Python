0f8e897c52992e0ca7b54db0bc3228c537b400c4
aksungurlu <t.aksungurlu@gmail.com>
2016-12-08 23:43:38 +0300

    Arrows added

--- GUI.py
+++ GUI.py
@@ -7,13 +7,25 @@ import tkMessageBox
 top = Tkinter.Tk()
 
 # Button to pop up
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
+def upMessage():
+   tkMessageBox.showinfo( "Hello Python", "Up Button Pressed")
+   
+def leftMessage():
+   tkMessageBox.showinfo( "Hello Python", "Left Button Pressed")
+   
+def downMessage():
+   tkMessageBox.showinfo( "Hello Python", "Down Button Pressed")
+   
+def rightMessage():
+   tkMessageBox.showinfo( "Hello Python", "Right Button Pressed")
 
-B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+B_up = Tkinter.Button(top, text = u'\u25b2', command = upMessage).pack()
+B_left = Tkinter.Button(top, text = u'\u24b2', command = leftMessage).pack()
+B_down = Tkinter.Button(top, text = u'\u23b2', command = downMessage).pack()
+B_right = Tkinter.Button(top, text = u'\u22b2', command = rightMessage).pack()
 
-B.pack()
-B.place(bordermode=OUTSIDE, height=100, width=100)
+#B_up.pack()
+#B.place(bordermode=OUTSIDE, relheight=0.2, relwidth=0.5)
 
 #B.grid(row=2,column=2)
 top.mainloop()

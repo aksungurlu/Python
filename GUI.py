c9aa696ae60ca8a5d449052f80cdb11d7c192d09
aksungurlu <t.aksungurlu@gmail.com>
2016-12-08 23:28:09 +0300

    place method on main window

--- GUI.py
+++ GUI.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 import Tkinter
+from Tkinter import *
+import tkMessageBox
+
 top = Tkinter.Tk()
-# Code to add widgets will go here...
+
+# Button to pop up
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+B.place(bordermode=OUTSIDE, height=100, width=100)
+
+#B.grid(row=2,column=2)
 top.mainloop()

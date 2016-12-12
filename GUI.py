ae64c982778f14a2dd91b4eef791ad017e1633e7
aksungurlu <t.aksungurlu@gmail.com>
2016-12-12 22:05:46 +0300

    Layout is better

--- GUI.py
+++ GUI.py
@@ -4,25 +4,40 @@ import Tkinter
 from Tkinter import *
 import tkMessageBox
 
+speed = 0
+direction = 0
+
 top = Tkinter.Tk()
 
 # Button to pop up
 def upMessage():
-   tkMessageBox.showinfo( "Hello Python", "Up Button Pressed")
+	global speed
+	if speed < 1:
+		speed += 0.1
    
 def leftMessage():
-   tkMessageBox.showinfo( "Hello Python", "Left Button Pressed")
+	global direction
+	if direction > -1:
+		direction -= 0.1
    
 def downMessage():
-   tkMessageBox.showinfo( "Hello Python", "Down Button Pressed")
+	global speed
+	if speed > -1:
+		speed -= 0.1
    
 def rightMessage():
-   tkMessageBox.showinfo( "Hello Python", "Right Button Pressed")
+	global direction
+	if direction < 1:
+		direction += 0.1
+   
+def showMessage():
+   tkMessageBox.showinfo( "Hello Python", "Speed:"+str(speed)+" Direction:"+str(direction))
 
-B_up = Tkinter.Button(top, text = u'\u25b2', command = upMessage).pack()
-B_left = Tkinter.Button(top, text = u'\u24b2', command = leftMessage).pack()
-B_down = Tkinter.Button(top, text = u'\u23b2', command = downMessage).pack()
-B_right = Tkinter.Button(top, text = u'\u22b2', command = rightMessage).pack()
+B_up = Tkinter.Button(top, text = u'\u25b2', command = upMessage).grid(row=1,column=2)
+B_left = Tkinter.Button(top, text = u'\u25c0', command = leftMessage).grid(row=2,column=1)
+B_down = Tkinter.Button(top, text = u'\u25bc', command = downMessage).grid(row=2,column=2)
+B_right = Tkinter.Button(top, text = u'\u25b6', command = rightMessage).grid(row=2,column=3)
+B_show = Tkinter.Button(top, text = "Show", command = showMessage).grid(row=4,column=2,rowspan=3)
 
 #B_up.pack()
 #B.place(bordermode=OUTSIDE, relheight=0.2, relwidth=0.5)

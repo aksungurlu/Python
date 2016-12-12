b4db3d1ee9d3a01a5102ccaa272dd60cfe9635d4
aksungurlu <t.aksungurlu@gmail.com>
2016-12-13 00:36:49 +0300

    bluetooth py is added

---
+++ Bluetooth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+import bluetooth
+
+target_name = "Tuncay"
+target_address = None
+
+nearby_devices = bluetooth.discover_devices()
+
+for bdaddr in nearby_devices:
+	#print(str(bluetooth.lookup_name(bdaddr)))
+    if target_name == bluetooth.lookup_name(bdaddr):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print "found target bluetooth device with address ", target_address
+else:
+    print "could not find target bluetooth device nearby"
